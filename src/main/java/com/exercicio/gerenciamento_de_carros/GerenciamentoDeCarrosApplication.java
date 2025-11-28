7f34e294a4fad6b581448ecdbfe09388de97f5a6
Rique22 <henrique@cnia.com.br>
2025-11-28 14:51:25 -0300

    Comentando main

--- src/main/java/com/exercicio/gerenciamento_de_carros/GerenciamentoDeCarrosApplication.java
+++ src/main/java/com/exercicio/gerenciamento_de_carros/GerenciamentoDeCarrosApplication.java
@@ -3,6 +3,10 @@ package com.exercicio.gerenciamento_de_carros;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+* Projeto de gerenciamento de carros
+* @author Henrique Bernnardo
+* */
 @SpringBootApplication
 public class GerenciamentoDeCarrosApplication {
 
